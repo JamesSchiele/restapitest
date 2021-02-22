a2fdc7899e87c496a995992dd0f22e2c471642ea
James <jamesschiele@hotmail.com>
2021-02-22 20:36:44 +0000

    Class for method/ end points started

--- src/run.py
+++ src/run.py
@@ -1,3 +1,3 @@
-from team_registry import app 
+from team_registry import app
 
-print("Hello")
+app.run(host='0.0.0.0', port=80, debug=True)
--- src/team_registry/__ini__.py
+++ src/team_registry/__ini__.py
@@ -1,15 +1,29 @@
 import markdown
 import os
+import shelve
 
 # Import the framework
-from flask import Flask
+from flask import Flask 
 
 # Create an insance of Flask
 app = Flask(_name_)
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open("teammates.db")
+    return db
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
 @app.route("/")
 def index():
-    """Present some documentation"""
+    """Present some documentation - README.txt"""
     
     # Open the README file
     with open(os.path.dirname(app.root_path) + '/README.txt', 'r') as markdown_file: # open README file from app root
@@ -19,3 +33,16 @@ def index():
 
         # Convert to HTML
         return markdown.markdown(content)
+
+ # create a class for each endpoint
+class TeammateList(Resource):
+    def get(self): # get method for getting team mate list
+        shelf = get_db()
+        members = list(shelf.keys())
+
+        team = [] # array of team members
+
+        for member in members:
+            team.append(shelf[member]) # loop over all keys in team member list, put in array and append to team array
+        
+        return {'message':'Success', 'data': team} # wrap in data field to add message
